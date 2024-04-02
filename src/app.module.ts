0c6363fe1457d7ae37271e3223898f505bab3649
Admin <support@unlimitpotential.com>
2024-04-01 20:38:37 -0400

    j

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,41 +1,43 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
-import { UploadModule } from './upload/upload.module'; // Import the UploadModule
+import { GraphQLModule } from '@nestjs/graphql'; // Import the GraphQLModule
+import { UploadModule } from './upload/upload.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BucketModule } from './bucket/bucket.module'; // Import the BucketModule
+import { BucketModule } from './bucket/bucket.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { OtpModule } from './otp/otp.module';
-import { UserModule } from './user/user.module'; // Import the UserModule
-import { CheckUserController } from './store/user.controller'; // Import the DataController
-import { WebsiteController } from './upload/upload.controller'; // Update the path accordingly
+import { UserModule } from './user/user.module';
+import { CheckUserController } from './store/user.controller';
+import { WebsiteController } from './upload/upload.controller';
 import { ChatCompletionController } from './chat-completion/chat-completion.controller';
 import { ChatCompletionService } from './chat-completion/chat-completion.service';
 import { ChatCompletionModule } from './chat-completion/chat-completion.module';
 import { AppGateway } from './app.gateway';
 import { EventsService } from './events.service';
-import { MediaController } from './media.controller'; // Import the MediaController
+import { MediaController } from './media.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     HttpModule,
     OtpModule,
-    UploadModule, // Add the UploadModule here
-    BucketModule, // Add the BucketModule here
+    UploadModule,
+    BucketModule,
     MulterModule.register({
-      dest: './uploads', // Destination folder for uploaded files
+      dest: './uploads',
     }),
-    UserModule, // Add the UserModule here
+    UserModule,
     ChatCompletionModule,
+    GraphQLModule.forRoot({ autoSchemaFile: true }), // Configure GraphQLModule here
   ],
   controllers: [
     AppController,
     CheckUserController,
     WebsiteController,
-    ChatCompletionController, // Include the DataController here
-    MediaController, // Add the MediaController here
+    ChatCompletionController,
+    MediaController,
   ],
   providers: [EventsService, AppService, ChatCompletionService, AppGateway],
 })
