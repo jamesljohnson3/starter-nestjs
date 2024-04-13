8e4494f34a0f09df5e54d1797cd586b6c95d8a73
Admin <support@unlimitpotential.com>
2024-04-13 06:28:47 -0400

    kk

--- src/stream-email/stream-email.controller.ts
+++ src/stream-email/stream-email.controller.ts
@@ -1,33 +1,29 @@
 // stream-email.controller.ts
 import { Controller, Get, Res } from '@nestjs/common';
-import { Response } from 'express';
-import * as AWS from 'aws-sdk';
+import axios from 'axios';
 
 @Controller('stream-email')
-export class StreamEmailController {
+export class EmailStreamController {
   @Get()
-  async streamEmail(@Res() res: Response): Promise<void> {
-    const s3 = new AWS.S3({
-      endpoint: 'https://s3.us-west-004.backblazeb2.com',
-      // Set access key and secret key for authorization
-      credentials: {
-        accessKeyId: '004c793eb828ace0000000004',
-        secretAccessKey: 'K004H1NvDQ+d9fD9sy9iBsYzd8f4/r8',
-      },
-    });
+  async getEmailStream(@Res() res) {
+    try {
+      const fileUrl =
+        'https://ok767777.s3.us-west-004.backblazeb2.com/Sent.mbox';
+      const response = await axios.get(fileUrl, { responseType: 'stream' });
 
-    const s3Params = {
-      Bucket: 'ok767777', // Update with your S3 bucket name
-      Key: 'Sent.mbox', // Update with your S3 file key
-    };
+      res.setHeader('Content-Type', 'text/plain');
 
-    const s3Stream = s3.getObject(s3Params).createReadStream();
+      // Stream file in chunks of 1MB
+      response.data.on('data', (chunk) => {
+        res.write(chunk);
+      });
 
-    res.setHeader(
-      'Content-Disposition',
-      'attachment; filename="All_mail_Including_Spam_and_Trash.mbox"',
-    );
-
-    s3Stream.pipe(res);
+      response.data.on('end', () => {
+        res.end();
+      });
+    } catch (error) {
+      console.error('Error streaming file:', error);
+      res.status(500).send({ error: 'Failed to stream file' });
+    }
   }
 }
