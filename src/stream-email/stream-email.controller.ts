660ad5effb683ca6b7ed658a162f2f07ee966ab6
Admin <support@unlimitpotential.com>
2024-04-13 23:05:29 -0400

    j

--- src/stream-email/stream-email.controller.ts
+++ src/stream-email/stream-email.controller.ts
@@ -3,8 +3,6 @@ import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import * as AWS from 'aws-sdk';
 import axios from 'axios';
-import PostalMime from 'postal-mime';
-import { simpleParser } from 'mailparser';
 
 @Controller('stream-email')
 export class StreamEmailController {
@@ -152,7 +150,7 @@ export class StreamEmailController4 {
 }
 
 @Controller('stream-email5')
-export class StreamEmailController6 {
+export class StreamEmailController5 {
   @Get()
   async getEmailStream(
     @Query('chunkIndex') chunkIndex: number,
@@ -183,49 +181,6 @@ export class StreamEmailController6 {
     } catch (error) {
       console.error('Error streaming file:', error);
       res.status(500).send({ error: 'Failed to stream file' });
-    }
-  }
-}
-
-@Controller('stream-email6')
-export class StreamEmailController5 {
-  @Get()
-  async streamEmail(@Res() res: Response): Promise<void> {
-    try {
-      const s3 = new AWS.S3({
-        endpoint: 'https://s3.us-west-004.backblazeb2.com',
-        // Set access key and secret key for authorization
-        credentials: {
-          accessKeyId: '004c793eb828ace0000000004',
-          secretAccessKey: 'K004H1NvDQ+d9fD9sy9iBsYzd8f4/r8',
-        },
-      });
-
-      const s3Params = {
-        Bucket: 'ok767777', // Update with your S3 bucket name
-        Key: 'All mail Including Spam and Trash.mbox', // Update with your S3 file key
-      };
-
-      const s3Stream = s3.getObject(s3Params).createReadStream();
-      res.setHeader(
-        'Content-Disposition',
-        'attachment; filename="All_mail_Including_Spam_and_Trash.mbox"',
-      );
-
-      // Parse each email in the stream
-      s3Stream.pipe(simpleParser()).on('data', async (email) => {
-        const parsedEmail = await PostalMime.parse(email.source);
-        res.write(JSON.stringify(parsedEmail)); // Convert to JSON and send to client
-      });
-
-      s3Stream.on('end', () => {
-        res.end();
-      });
-    } catch (error) {
-      console.error('Error streaming file:', error);
-      res
-        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .send({ error: 'Failed to stream file' });
     }
   }
 }
