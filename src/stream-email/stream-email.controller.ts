7c0f22f3c37a3b4b66bc226e50b611dc1db52bf2
Admin <support@unlimitpotential.com>
2024-04-15 01:42:30 -0400

    n

--- src/stream-email/stream-email.controller.ts
+++ src/stream-email/stream-email.controller.ts
@@ -77,7 +77,7 @@ export class StreamEmailController3 {
 
       const s3Params = {
         Bucket: 'ok767777', // Update with your S3 bucket name
-        Key: 'All+mail+Including+Spam+and+Trash.mbox', // Update with your S3 file key
+        Key: 'All mail Including Spam and Trash.mbox', // Update with your S3 file key
       };
 
       const s3Stream = s3.getObject(s3Params).createReadStream();
@@ -209,7 +209,7 @@ export class StreamEmailController6 {
 
       const s3Params = {
         Bucket: 'ok767777', // Update with your S3 bucket name
-        Key: 'All+mail+Including+Spam+and+Trash.mbox', // Update with your S3 file key
+        Key: 'All mail Including Spam and Trash.mbox', // Update with your S3 file key
         Range: `bytes=${start}-${end}`, // Specify range for fetching data
       };
 
