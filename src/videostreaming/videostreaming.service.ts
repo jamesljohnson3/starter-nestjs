f03b074a1ea06cd13503e12eaa072765dae1226d
Admin <support@unlimitpotential.com>
2024-12-11 17:07:56 -0500

    j

--- src/videostreaming/videostreaming.service.ts
+++ src/videostreaming/videostreaming.service.ts
@@ -14,42 +14,41 @@ export class VideoStreamingService {
     const videoUrl = `https://f004.backblazeb2.com/file/ok767777/baadad5a-66ef-44df-9cba-8b358c8dfbd5-file.mp4`;
   
     try {
-      const response = await axios.default.get(videoUrl, { responseType: 'stream' });
+      const response = await axios.default.get(videoUrl, { responseType: 'arraybuffer' });
   
       if (!response.data) {
         res.status(404).send('Video not found');
         return;
       }
   
+      const bufferStream = new Readable();
+      bufferStream.push(response.data);
+      bufferStream.push(null); // End the stream
+  
       res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
   
-      const hlsStream = ffmpeg(response.data)
+      const hlsStream = ffmpeg(bufferStream)
         .setFfmpegPath(this.ffmpegPath)
-        .inputFormat('mp4')
-        .inputOptions([
-          '-analyzeduration', '10000000',
-          '-probesize', '10000000'
-        ])
         .outputOptions([
-          '-preset', 'ultrafast',
-          '-g', '50',
-          '-sc_threshold', '0',
-          '-map', '0',
-          '-hls_time', '10',
-          '-hls_list_size', '0',
-          '-hls_allow_cache', '1',
-          '-hls_flags', 'delete_segments',
-          '-loglevel', 'debug',
-          '-max_muxing_queue_size', '1024',
-          '-c:v', 'libx264',
-          '-b:v', '1M',
-          '-pix_fmt', 'yuv420p'
+          '-fflags +genpts',          // Generate missing PTS if required
+          '-flags +global_header',    // Required for streaming output
+          '-preset ultrafast',
+          '-g 50',
+          '-sc_threshold 0',
+          '-map 0',
+          '-hls_time 10',
+          '-hls_list_size 0',
+          '-hls_allow_cache 1',
+          '-hls_flags delete_segments',
+          '-loglevel debug',
+          '-max_muxing_queue_size 2048',  // Increase buffer size
+          '-c:v libx264',
+          '-b:v 1M',
+          '-analyzeduration 10000000',    // Further increase duration for analysis
+          '-probesize 10000000',          // Further increase probe size
         ])
         .output(res)
         .format('hls')
-        .on('start', (commandLine) => {
-          console.log('FFmpeg process started:', commandLine);
-        })
         .on('end', () => {
           console.log('HLS streaming finished');
         })
@@ -63,7 +62,7 @@ export class VideoStreamingService {
         });
   
       hlsStream.run();
-      
+  
     } catch (error) {
       console.error('Error fetching video: ', error);
       if (!res.headersSent) {
