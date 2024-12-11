362dfa710c50ab476f7518dd3a9d85164214f397
Admin <support@unlimitpotential.com>
2024-12-11 12:02:41 -0500

    j

--- src/videostreaming/videostreaming.controller.ts
+++ src/videostreaming/videostreaming.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res, Query } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { VideoStreamingService } from './videostreaming.service';
 import { Response } from 'express';
 
@@ -7,13 +7,14 @@ export class VideoStreamingController {
   constructor(private readonly videoStreamingService: VideoStreamingService) {}
 
   // Endpoint to stream video by ID
-  @Get('video')
-  async streamVideo(@Query('id') id: string, @Res() res: Response) {
+  @Get('video/:id')  // Use route parameter ':id' instead of query parameter
+  async streamVideo(@Param('id') id: string, @Res() res: Response) {
     if (!id) {
       return res.status(400).send('Video ID is required');
     }
 
     try {
+      // Pass the ID and response object to the service for processing
       await this.videoStreamingService.streamVideo(id, res);
     } catch (error) {
       res.status(500).send('Error streaming video');
